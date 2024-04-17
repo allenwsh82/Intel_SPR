782efb19331e661be1035598c615ffa5ebf07412
DarkHorse <44453417+allenwsh82@users.noreply.github.com>
2024-04-17 18:00:43 +0800

    Update inference_with_INT4.sh

--- SFTT/inference_with_INT4.sh
+++ SFTT/inference_with_INT4.sh
@@ -1,1 +1,1 @@
-numactl -C 0-55,112-167  python inference_INT4.py --prompt "What are some unique things about the 37th largest city in Japan?" --n-predict 200
+numactl -C 0-55,112-167  python inference_with_INT4.py --prompt "What are some unique things about the 37th largest city in Japan?" --n-predict 200
