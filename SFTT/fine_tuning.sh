fa4f12ded135b7f8ed456911743a80e8b65f2b6d
DarkHorse <44453417+allenwsh82@users.noreply.github.com>
2024-05-16 07:21:31 +0800

    Update fine_tuning.sh

--- SFTT/fine_tuning.sh
+++ SFTT/fine_tuning.sh
@@ -1,2 +1,4 @@
-export OMP_NUM_THREADS=224
-python fine_tuning.py --bf16 True --use_ipex True --max_seq_length 512
+#export OMP_NUM_THREADS=224
+export OMP_NUM_THREADS=112
+
+numactl -C 0-55,112-167 python fine_tuning.py --bf16 True --use_ipex True --max_seq_length 512
