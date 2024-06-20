138231747163674bc31b121d3e472bf8046055ea
DarkHorse <44453417+allenwsh82@users.noreply.github.com>
2024-06-20 12:27:13 +0800

    Update fine_tuning.py

--- SFTT/fine_tuning.py
+++ SFTT/fine_tuning.py
@@ -15,7 +15,7 @@ def main(FLAGS):
     dataset = load_dataset("mlabonne/guanaco-llama2-1k", split="train")
 
     #Get your hf_token from huggingface.co webpage
-    #Use this hf token if yours can't work:    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
+    #Use this hf token if yours can't work:    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
     hf_token = " "   
     
     
