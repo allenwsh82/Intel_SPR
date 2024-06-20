e42edd8ae1cd406b28648a0ff020d722a6f4b20f
DarkHorse <44453417+allenwsh82@users.noreply.github.com>
2024-06-20 23:02:07 +0800

    Update inference_with_IPEX.sh

--- SFTT/inference_with_IPEX.sh
+++ SFTT/inference_with_IPEX.sh
@@ -1,1 +1,2 @@
 numactl -C 0-55,112-167  python Inference_with_IPEX.py --prompt "what is the total Nike earnings before interest and taxes in fiscal year 2022 in Europe, Middle East and Africa?"
+#numactl -C 0-47,96-143  python Inference_with_IPEX.py --prompt "what is the total Nike earnings before interest and taxes in fiscal year 2022 in Europe, Middle East and Africa?
