0fd17243039c7054f4078c66358aa77a6b1532ef
DarkHorse <44453417+allenwsh82@users.noreply.github.com>
2024-06-20 23:04:22 +0800

    Update inference_with_INT4.sh

--- SFTT/inference_with_INT4.sh
+++ SFTT/inference_with_INT4.sh
@@ -1,5 +1,3 @@
 #numactl -C 0-55,112-167  python inference_with_INT4.py --prompt "What are some unique things about the 37th largest city in Japan?" --n-predict 200
-#numactl -C 0-47,96-143  python inference_with_INT4.py --prompt "What is the total Nike earnings before interest and taxes in fiscal year 2022 in Europe, Middle East and Africa?
-" --n-predict 200
-numactl -C 0-55,112-167  python inference_with_INT4.py --prompt "What is the total Nike earnings before interest and taxes in fiscal year 2022 in Europe, Middle East and Africa?
-" --n-predict 200
+#numactl -C 0-47,96-143  python inference_with_INT4.py --prompt "What is the total Nike earnings before interest and taxes in fiscal year 2022 in Europe, Middle East and Africa?" --n-predict 200
+numactl -C 0-55,112-167  python inference_with_INT4.py --prompt "What is the total Nike earnings before interest and taxes in fiscal year 2022 in Europe, Middle East and Africa?" --n-predict 200
